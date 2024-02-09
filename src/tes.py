bc266f9fc3e95ae7f006ecc108b70e3e1d5ae34f
FedrianzD <fedrianzd@gmail.com>
2024-02-10 00:53:42 +0700

    fix: main and function

---
+++ src/tes.py
@@ -0,0 +1,13 @@
+import os.path
+import numpy as np
+file = os.path.isfile("../test/file.txt")
+print(os.getcwd())
+print(file)
+
+buffer = [[1,2,3,4], [1,2,4,5], [2,4,5,6], [2,4,5,6]]
+for i in range(len(buffer)-1):
+    print(buffer[i])
+    print(buffer[i+1:])
+    if buffer[i] in buffer[i+1:]:
+        print("duplicate")
+        break
