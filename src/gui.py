2ff415b81d702b80f330cc9a0ea57c275c2ef1c3
FedrianzD <fedrianzd@gmail.com>
2024-02-12 23:40:35 +0700

    feat: gui

--- src/gui.py
+++ src/gui.py
@@ -3,6 +3,7 @@ from tkinter import filedialog
 from tkinter import messagebox
 import function
 import time
+import random
 maxscore = ""
 buffer = ""
 coordinate = ""
@@ -12,9 +13,8 @@ matrix = []
 label_matrix = []
 def openFile():
     global maxscore, buffer, coordinate, finish, start, matrix, label_matrix
-    for label_row in label_matrix:
-            for label in label_row:
-                label.destroy()
+    canvas.delete('all')
+    canvas2.delete('all')
     filepath = filedialog.askopenfilename(initialdir="D:\\Github\\Tucil1_13522090\\test")
     file = open(filepath, 'r')
     while True:
@@ -76,20 +76,36 @@ def openFile():
     maxscore, buffer, coordinate = function.bruteForce(matrix, matrix_sequence, buffer_size)
     finish = time.time()
     maxscore2.set(maxscore)
-    buffer2.set(buffer)
-    time2.set(f"{finish-start:.2f} ms")
+    cell_size = 50
     for i in range(len(matrix)):
-        label_row = []
         for j in range(len(matrix[0])):
-            if (j+1, i+1) in coordinate:
-                label = Label(frame3, text=matrix[i][j], borderwidth=1, relief="solid", width=10, height=4, bg='red')
-            else:
-                label = Label(frame3, text=matrix[i][j], borderwidth=1, relief="solid", width=10, height=4)
-            label.grid(row=i, column=j)
-            label_row.append(label)
-        label_matrix.append(label_row)
+            y1 = i*cell_size + 10
+            x1 = j*cell_size + 10
+            y2 = y1 + cell_size
+            x2 = x1 + cell_size
+            canvas.create_rectangle(x1,y1,x2,y2, outline='green', width=2)
+            canvas.create_text(x1+25, y1+25, text=str(matrix[i][j]))
+    if maxscore != 0:
+        buffer2.set(buffer)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                y1 = i*cell_size + 10
+                x1 = j*cell_size + 10
+                y2 = y1 + cell_size
+                x2 = x1 + cell_size
+                if (j+1,i+1) in coordinate:
+                    canvas.create_rectangle(x1,y1,x2,y2, outline='red', width=4)
+        for i in range(len(coordinate)-1):
+            x1 = (coordinate[i][0] - 1) * cell_size + 35
+            y1 = (coordinate[i][1] - 1) * cell_size + 35
+            x2 = (coordinate[i+1][0] - 1) * cell_size + 35
+            y2 = (coordinate[i+1][1] - 1) * cell_size + 35
+            canvas.create_line(x1, y1, x2, y2, fill='red', width=3)
+    else:
+        buffer2.set('No solution')
+    time2.set(f"{finish-start:.2f} ms")
     
-
+    
 def saveFile():
     global maxscore, buffer, coordinate, finish, start
     file = filedialog.asksaveasfile(defaultextension='.txt')
@@ -106,41 +122,196 @@ def saveFile():
     file.write('\n') 
     file.close()
 
+def submit():
+    global maxscore, buffer, coordinate, finish, start, matrix, label_matrix
+    canvas.delete('all')
+    canvas2.delete('all')
 
-    
+    token_unik = entry1.get()
+    token = entry2.get()
+    buffer_size = entry3.get()
+    kolom = entry4.get()
+    baris = entry4b.get()
+    jumlah_seq = entry5.get()
+    ukuran_max_seq = entry6.get()
+
+    try:
+        token_unik = int(token_unik)
+        buffer_size = int(buffer_size)
+        kolom = int(kolom)
+        baris = int(baris)
+        jumlah_seq = int(jumlah_seq)
+        ukuran_max_seq = int(ukuran_max_seq)
+    except:
+        messagebox.showerror(title='Something is wrong', message="Harus berupa integer.\n")
+    if int(token_unik) < 2:
+        messagebox.showerror(title='Something is wrong', message="Jumlah token harus lebih dari 1.\n")
+    if int(token_unik) <= 0:
+        messagebox.showerror(title='Something is wrong', message="Jumlah token unik tidak boleh 0 atau negatif.")
+    token = token.split()
+    isAlphaNum = all(element.isalnum() for element in token)
+    isLen2 = all(len(element) == 2 for element in token)
+    if len(set(token)) != token_unik or not(isAlphaNum) or not(isLen2):
+        messagebox.showerror(title='Something is wrong', message="Token harus berupa alphanumeric dan berjumlah sama dengan jumlah token unik")
+    if int(buffer_size) < 2:
+        messagebox.showerror(title='Something is wrong', message="Ukuran buffer minimal adalah 2.")
+    if int(kolom) <= 0 or int(baris) <= 0 or ((int(kolom) * int(baris)) < token_unik):
+        messagebox.showerror(title='Something is wrong', message="Ukuran matrix tidak boleh 0 ataupun negatif dan ukuran matrix harus lebih besar dari jumlah token unik.")
+    jumlah_seq = int(jumlah_seq)
+    if jumlah_seq <= 0:
+        messagebox.showerror(title='Something is wrong', message="Jumlah sequence minimal 1.")
+    ukuran_max_seq = int(ukuran_max_seq)
+    if ukuran_max_seq < 2:
+        messagebox.showerror(title='Something is wrong', message="Panjang minimal sequence adalah 2.")
+    matrix = [[0 for i in range(int(kolom))] for j in range(int(baris))]
+    minimumlength = min(kolom, baris)
+    for item in token:
+        x = random.randint(0, len(matrix)-1)
+        y = random.randint(0, len(matrix[0])-1)
+        while matrix[x][y] != 0:
+            x = random.randint(0, len(matrix)-1)
+            y = random.randint(0, len(matrix[0])-1)
+        matrix[x][y] = item
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] != 0:
+                continue
+            else:
+                matrix[i][j] = random.choice(token)
+    isUnique = False
+    matrix_sequence = [[random.choice(token) for i in range(random.randint(2,ukuran_max_seq))] for j in range(jumlah_seq)]
+    reroll = False
+    while not(isUnique):
+        for i in range(len(matrix_sequence)-1):
+            if matrix_sequence[i] in matrix_sequence[i+1:]:
+                matrix_sequence = [[random.choice(token) for bla in range(random.randint(2,ukuran_max_seq))] for blabla in range(jumlah_seq)]
+                reroll = True
+                break
+        if not reroll:
+            isUnique = True
+        reroll = False
+    for item in matrix_sequence:
+        item.append(random.randint(1,100))
+    start = time.time()
+    maxscore, buffer, coordinate = function.bruteForce(matrix, matrix_sequence, buffer_size)
+    finish = time.time()
+    maxscore2.set(maxscore)
+    buffer2.set(buffer)
+    time2.set(f"{finish-start:.2f} ms")
+    cell_size = 50
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            y1 = i*cell_size + 10
+            x1 = j*cell_size + 10
+            y2 = y1 + cell_size
+            x2 = x1 + cell_size
+            if (j+1,i+1) not in coordinate:
+                canvas.create_rectangle(x1,y1,x2,y2, outline='green', width=2)
+            canvas.create_text(x1+25, y1+25, text=str(matrix[i][j]))
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            y1 = i*cell_size + 10
+            x1 = j*cell_size + 10
+            y2 = y1 + cell_size
+            x2 = x1 + cell_size
+            if (j+1,i+1) in coordinate:
+                canvas.create_rectangle(x1,y1,x2,y2, outline='red', width=4)
+    for i in range(len(coordinate)-1):
+        x1 = (coordinate[i][0] - 1) * cell_size + 35
+        y1 = (coordinate[i][1] - 1) * cell_size + 35
+        x2 = (coordinate[i+1][0] - 1) * cell_size + 35
+        y2 = (coordinate[i+1][1] - 1) * cell_size + 35
+        canvas.create_line(x1, y1, x2, y2, fill='red', width=3)
+    cell_size2 = 20
+    for i in range(len(matrix_sequence)):
+        for j in range(len(matrix_sequence[i])):
+            y1 = i*cell_size2 + 10
+            x1 = j*cell_size2 + 10
+            y2 = y1 + cell_size2
+            x2 = x1 + cell_size2
+            if j != len(matrix_sequence[i])-1:
+                canvas2.create_rectangle(x1,y1,x2,y2, outline='green', width=2)
+            canvas2.create_text(x1+10, y1+10, text=str(matrix_sequence[i][j]))
+    for i in range(len(matrix_sequence)):
+        for j in range(len(matrix_sequence[i])):
+            y1 = i*cell_size2 + 10
+            x1 = j*cell_size2 + 10
+            y2 = y1 + cell_size2
+            x2 = x1 + cell_size2
+            if j == len(matrix_sequence[i])-1:
+                canvas2.create_rectangle(x1,y1,x2,y2, outline='red', width=2)
+            canvas2.create_text(x1+10, y1+10, text=str(matrix_sequence[i][j]))
 
 window = Tk()
-window.geometry("800x800")
+window.geometry("900x800")
+window.title("Breach Protocol")
 window.config(bg="Black")
 frame = Frame(window)
-frame.config(bg="black")
+frame.config(bg="white")
 frame2 = Frame(window)
 frame2.config(bg="black")
 
 frame3 = Frame(window)
 frame3.config(bg='black')
 
+canvas = Canvas(frame3, width=400, height=400)
+canvas.pack()
+frame4 = Frame(window)
+frame4.config(bg='black')
+
+frame5 = Frame(window)
+frame5.config(bg='white')
+
+labelseq = Label(frame5, text="Sequence: ", width=20)
+labelseq.pack(side=TOP)
+canvas2 = Canvas(frame5, width=200, height=300)
+canvas2.pack()
+
+entry1 = Entry(frame4)
+entry1.grid(row=0,column=1)
+labelentry1 = Label(frame4, text='Jumlah token: ', width=25).grid(row=0,column=0, padx=5)
+entry2 = Entry(frame4)
+entry2.grid(row=1,column=1)
+labelentry2 = Label(frame4, text='Token unik: ', width=25).grid(row=1,column=0, padx=5)
+entry3 = Entry(frame4)
+entry3.grid(row=2,column=1)
+labelentry3 = Label(frame4, text='Ukuran buffer: ', width=25).grid(row=2,column=0, padx=5)
+entry4 = Entry(frame4)
+entry4.grid(row=3,column=1)
+labelentry4a = Label(frame4, text='Panjang kolom: ', width=25).grid(row=3,column=0, padx=5)
+entry4b = Entry(frame4)
+entry4b.grid(row=4,column=1)
+labelentry4b = Label(frame4, text='Panjang baris: ', width=25).grid(row=4,column=0, padx=5)
+entry5 = Entry(frame4)
+entry5.grid(row=5,column=1)
+labelentry5 = Label(frame4, text='Jumlah sequence: ', width=25).grid(row=5,column=0, padx=5)
+entry6 = Entry(frame4)
+entry6.grid(row=6,column=1)
+labelentry6 = Label(frame4, text='Panjang maksimal sequence: ', width=25).grid(row=6,column=0, padx=5)
 
 fileButton = Button(frame, text="BROWSE", command=openFile, width=5, padx=10).grid(column=0, row= 0, pady=5, padx=10)
 saveButton = Button(frame, text='Save', command=saveFile,width=5, padx=10).grid(column=0, row= 5, pady=5, padx=10)
+saveButton = Button(frame4, text='Submit', command=submit,width=5, padx=10).grid(column=0, row= 7, pady=5, padx=10)
 
 maxscore2 = StringVar()
 labelmaxscore = Label(frame2, text='Reward: ', fg="green", width=10).grid(column=0, row= 0, pady=5, padx=10)
-label1 = Label(frame2, textvariable=maxscore2, width=10).grid(column=0, row= 3, pady=(5,50), padx=10)
+label1 = Label(frame2, textvariable=maxscore2, width=10).grid(column=0, row= 3, pady=(5,5), padx=10)
 
 buffer2 = StringVar()
 labelbuffer = Label(frame2, text='Solution: ', fg="green", width=30).grid(column=5, row= 0, pady=5, padx=10)
-label2 = Label(frame2, textvariable=buffer2, width=30).grid(column=5, row= 3, pady=(5,50), padx=10)
+label2 = Label(frame2, textvariable=buffer2, width=30).grid(column=5, row= 3, pady=(5,5), padx=10)
 
 time2 = StringVar()
 labeltime = Label(frame2, text='Time: ', fg="green", width=10).grid(column=10, row= 0, pady=5, padx=(10,20))
-label3 = Label(frame2, textvariable=time2, width=10).grid(column=10, row= 3, pady=(5,50), padx=(10,20))
+label3 = Label(frame2, textvariable=time2, width=10).grid(column=10, row= 3, pady=(5,5), padx=(10,20))
 
 
 
-frame.grid(column=0, row=5, padx=20)
-frame2.grid(column=3, row=0, pady=20)
-frame3.grid(column=3, row=5, ipadx=5, ipady=5)
+frame.grid(column=0, row=2, padx=20)
+frame2.grid(column=0, row=0, pady=20)
+frame3.grid(column=0, row=1, ipadx=5, ipady=5)
+frame4.grid(column=1, row=0, pady=20, padx=20)
+frame5.grid(column=1, row=1)
 
 
 window.mainloop() 
